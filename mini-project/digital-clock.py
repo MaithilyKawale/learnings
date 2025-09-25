696b135338ffbaad1570f58bdf447e60af9648a9
MaithilyKawale <maithilyworkspace@gmail.com>
2025-09-25 20:33:08 +0530

    mini project folder for digital clock created

---
+++ mini-project/digital-clock.py
@@ -0,0 +1,22 @@
+from tkinter import *
+import datetime
+
+def date_time():
+    time = datetime.datetime.now()
+    hr = time.strftime('%I')
+    mi = time.strftime('%M')
+    sec = time.strftime('%S')
+    am = time.strftime('%p')
+    date = time.strftime('%d')
+    month = time.strftime('%m')
+    year = time.strftime('%Y')
+    day = time.strftime('%A')
+
+    label_hr.config(text=hr)
+    label_min.config(text=mi)
+    label_sec.config(text=sec)
+    label_am.config(text=am)
+    label_date.config(text=date)
+    label_mo.config(text=month)
+    label_year.config(text=year)
+    label_day.config(text=day)
