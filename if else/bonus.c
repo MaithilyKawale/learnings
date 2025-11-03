a1e70f087ec3324cd6703ecc6854d2a7d3baae4b
MaithilyKawale <maithilyworkspace@gmail.com>
2025-11-03 21:39:29 +0530

    bonus.c file added

---
+++ if else/bonus.c
@@ -0,0 +1,5 @@
+/*A company decided to give bonus to employee according to following criteria
+more than 10 years  10%
+>=6 and <=10   8%
+less than 6 years     5%
+ask user for their salary and years of service and print the net bonus amount*/
