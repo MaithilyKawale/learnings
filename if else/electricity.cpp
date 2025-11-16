b569379afdf0ae95a2aebb19078c22034899f4ef
MaithilyKawale <maithilyworkspace@gmail.com>
2025-11-16 15:43:33 +0530

    electricity code file added

---
+++ if else/electricity.cpp
@@ -0,0 +1,4 @@
+/*write a program to calculate the electricity bill(accept number of unit from user) according to the following criteria
+first 100unit no charge
+next 100 unit rs 5 per unit
+next 200 unit rs 10 per unit*/
