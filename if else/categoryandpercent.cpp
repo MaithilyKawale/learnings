a943fedf4d03f07b6b37cef4a3b706a30bf205c8
MaithilyKawale <maithilyworkspace@gmail.com>
2025-11-18 21:53:27 +0530

    new file added

---
+++ if else/categoryandpercent.cpp
@@ -0,0 +1,5 @@
+/*write a program to accept percentage and display the category according to the following criteria
+<40     failed
+>=40 and <55          fair
+>=65  and  <65       good
+>=65   excellent*/
