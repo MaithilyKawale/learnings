5556c0f947817ffc2bfcb23c5697be437f4c0130
MaithilyKawale <maithilyworkspace@gmail.com>
2025-11-24 17:07:26 +0530

    file added

---
+++ if else/marketprice.cpp
@@ -0,0 +1,4 @@
+/*according the marked price from the user and calculate the net amount as(market price-discount) to pay according to following criteria
+>10000 20%
+>7000 and <=10000      15%
+<=7000      10%*/
