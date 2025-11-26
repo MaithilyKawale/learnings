6a1ddfc6367c1d39bd455a6d00b768cf3facc328
MaithilyKawale <maithilyworkspace@gmail.com>
2025-11-26 21:39:24 +0530

    file added for library charge code

---
+++ if else/librarycharge.cpp
@@ -0,0 +1,5 @@
+/*accept the number of days from the user and calculate the charge for library according to following
+till 5 days       2rs
+6 to 10 days        3rs
+11 to 15 days  4rs
+after 15days  5rs*/
