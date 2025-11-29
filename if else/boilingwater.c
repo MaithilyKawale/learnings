fa3ed653806c8e19d5ae86861bfdb11a35bd4c23
MaithilyKawale <maithilyworkspace@gmail.com>
2025-11-29 09:39:55 +0530

    boiling water code added

--- if else/boilingwater.c
+++ if else/boilingwater.c
@@ -1,1 +1,16 @@
 /*accept the temperature in degree celsius of water amd check whether it is boiling or not*/
+
+#include <stdio.h>
+
+int main(){
+    float temp;
+    printf("Enter temperature:");
+    scanf("%f",&temp);
+
+    if(temp>=100){
+        printf("water is boiling");
+    }
+    else{
+        printf("water is not boiling");
+    }
+}
