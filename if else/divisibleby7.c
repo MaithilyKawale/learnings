712bc0ee9f0585c2656aeb935ad66fe4bd53facf
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-01 11:03:18 +0530

    code file added to find number divisible by 7

--- if else/divisibleby7.c
+++ if else/divisibleby7.c
@@ -1,1 +1,17 @@
 /*write a program to check whether a num is divisible by 7 or not*/
+
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Enter any number:");
+    scanf("%d",&num);
+    
+    if(num%7==0){
+       printf("Number is divisible by 7");
+        
+    }
+    else{
+        printf("Number is not divisible by 7");
+    }
+}
