967823c375e21feac7e8b727efaf8999e5d47dfa
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-12 22:33:33 +0530

    code file added

--- if else/marketprice.c
+++ if else/marketprice.c
@@ -2,3 +2,29 @@
 >10000 20%
 >7000 and <=10000      15%
 <=7000      10%*/
+
+#include <stdio.h>
+int main() {
+    float marketPrice;
+    float discount;
+    float netAmount;
+
+    printf("Enter the market price: ");
+    scanf("%f", &marketPrice);
+
+    if (marketPrice > 10000) {
+        discount = marketPrice * 0.20;
+    }
+    else if (marketPrice > 7000 && marketPrice <= 10000) {
+        discount = marketPrice * 0.15;
+    }
+    else {
+        discount = marketPrice * 0.10;
+    }
+
+    netAmount = marketPrice - discount;
+
+    printf("Net amount to pay is: %.2f Rs\n", netAmount);
+
+    return 0;
+}
