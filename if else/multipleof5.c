9041d34312efec7287ff249fbcd2fa9549eea64d
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-12 22:32:53 +0530

    code file added

--- if else/multipleof5.c
+++ if else/multipleof5.c
@@ -1,1 +1,20 @@
 /*write a program to display "Hello" if a number entered by user is a multiple of five otherwise print "Bye"*/
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter num: ");
+    scanf("%d", &num);
+
+    if (num%5==0 && num>0) {
+        printf("Hello");
+    }
+
+    else{
+        printf("Bye");
+    }
+
+    return 0;
+}
