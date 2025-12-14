a69ec3d6cf87060ad6673adeff72eecdd5cab87b
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-14 22:16:51 +0530

    code added

--- if else/3digitornot.c
+++ if else/3digitornot.c
@@ -1,1 +1,18 @@
-/*write a python program to check whether a number entered is three digit number or not*/
+/*write a C program to check whether a number entered is three digit number or not*/
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Enter any num:");
+    scanf("%d",&num);
+    
+    if(num>99 && num<=999 || num==000){
+        printf("It is Three digit number.");
+    }
+    else if(num<-99 && num>=-999){
+        printf("It is Three digit number.");
+    }
+    else{
+        printf("It is not three digit number.");
+    }
+}
