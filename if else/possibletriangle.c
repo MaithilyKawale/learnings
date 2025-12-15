fb62a3c957c1317815a494f7b686a81152dd3782
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-15 12:59:37 +0530

    possible triangles code added

--- if else/possibletriangle.c
+++ if else/possibletriangle.c
@@ -1,2 +1,26 @@
 /*accept three sides of triangle and check whether the triangle is possible or not
 triangle is possible only when sum of any two sides is greater than 3rd side*/
+
+#include <stdio.h>
+
+int main() {
+    int side1, side2, side3;
+
+    printf("Enter side1: ");
+    scanf("%d", &side1);
+    
+    printf("Enter side2: ");
+    scanf("%d", &side2);
+    
+    printf("Enter side3: ");
+    scanf("%d", &side3);
+
+    if (side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2) {
+        printf("Triangle is possible");
+    }
+    else {
+        printf("Triangle is not possible");
+    }
+
+    return 0;
+}
