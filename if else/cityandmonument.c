3c19249e58798affd53217a3ec4a5f74505bb18e
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-16 20:29:21 +0530

    code added

--- if else/cityandmonument.c
+++ if else/cityandmonument.c
@@ -2,3 +2,24 @@
 delhi red fort
 agra taj mahal
 jaipur jal mahal*/
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char city[50];
+
+    printf("Enter the name of the city (delhi, agra, jaipur): ");
+    scanf("%s", city);
+
+    if (strcmp(city, "delhi") == 0) {
+        printf("Monument: Red Fort\n");
+    } else if (strcmp(city, "agra") == 0) {
+        printf("Monument: Taj Mahal\n");
+    } else if (strcmp(city, "jaipur") == 0) {
+        printf("Monument: Jal Mahal\n");
+    } else {
+        printf("Monument not found for the given city.\n");
+    }
+
+    return 0;
+}
