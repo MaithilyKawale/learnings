fdf9bb1a5f0c338def1d7d44d4c716e8c0365170
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-17 09:43:22 +0530

    code added

--- if else/grade.cpp
+++ if else/grade.cpp
@@ -3,3 +3,28 @@
 >80  <=90 B
 >=60 <=80 C
 below 60*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float percentage;
+
+    cout << "Enter your percentage: ";
+    cin >> percentage;
+
+    if (percentage > 90) {
+        cout << "Grade: A" << endl;
+    } 
+    else if (percentage > 80 && percentage <= 90) {
+        cout << "Grade: B" << endl;
+    } 
+    else if (percentage >= 60 && percentage <= 80) {
+        cout << "Grade: C" << endl;
+    } 
+    else {
+        cout << "Grade: Below C" << endl;
+    }
+
+    return 0;
+}
