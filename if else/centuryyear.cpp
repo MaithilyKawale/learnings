17b6a7cab17bc0df8f2aa671f4d0600e88b69b8b
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-21 22:56:11 +0530

    code added to find century year

--- if else/centuryyear.cpp
+++ if else/centuryyear.cpp
@@ -1,1 +1,17 @@
 /*Check if year is a century year*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int year;
+    cout<<"Enter any year:";
+    cin>>year;
+    
+    if(year%100==0 && year>0){
+        cout<<"It is century year.";
+    }
+    else{
+        cout<<"It is not century year.";
+    }
+}
