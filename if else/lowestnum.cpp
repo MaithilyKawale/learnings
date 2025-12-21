dc0aac98e0bb53af8e222a3d9ee2764b9d041ba4
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-21 22:56:46 +0530

    code added to find lowest num

--- if else/lowestnum.cpp
+++ if else/lowestnum.cpp
@@ -1,1 +1,27 @@
-/*write a python program to find the lowest number out of two number expected from user*/
+/*write a cpp program to find the lowest number out of two number expected from user*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout<<"Enter num1: ";
+    cin>>num1;
+    
+    cout<<"Enter num2: ";
+    cin>>num2;
+
+    if (num1 > num2) {
+        cout<<"num2 is smaller";
+    }
+    else if (num2 > num1) {
+        cout<<"num1 is smaller";
+    }
+    
+    else{
+        cout<<"Two of them is equal";
+    }
+
+    return 0;
+}
