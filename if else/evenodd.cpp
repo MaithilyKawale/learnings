79f6a651b4a19492b94bd1e55923db0c917a334a
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-23 00:35:46 +0530

    even odd code added

--- if else/evenodd.cpp
+++ if else/evenodd.cpp
@@ -1,1 +1,18 @@
 /*write a program to check whether a number entered by user is even or odd*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter any num:";
+    cin>>num;
+    
+    if(num%2==0){
+        cout<<"It is even number";
+    }
+    
+    else{
+        cout<<"It is odd number";
+    }
+}
