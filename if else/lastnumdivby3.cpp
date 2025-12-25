9e7e60058c6d6c5d660e7b4755d775de86de709f
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-25 22:32:30 +0530

    code added to find last num

--- if else/lastnumdivby3.cpp
+++ if else/lastnumdivby3.cpp
@@ -1,1 +1,20 @@
 /*write a program to check whether the last digit of a num is divisible by 3 or not*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    int num, lastDigit;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    lastDigit = num % 10;
+
+    if (lastDigit % 3 == 0) {
+        cout << "The last digit " << lastDigit << " is divisible by 3." << endl;
+    } else {
+        cout << "The last digit " << lastDigit << " is not divisible by 3." << endl;
+    }
+
+    return 0;
+}
