963d88eaa95342442a121fcdf81e689394373ed0
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-27 19:09:22 +0530

    code added

--- if else/categoryandpercent.cpp
+++ if else/categoryandpercent.cpp
@@ -1,5 +1,34 @@
 /*write a program to accept percentage and display the category according to the following criteria
-<40     failed
->=40 and <55          fair
->=65  and  <65       good
->=65   excellent*/
+>0 and <40     failed
+>=40 and <65          fair
+>=65  and  <75       good
+>=75 and <=100   excellent*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int percent;
+    cout<<"Enter your percentage: ";
+    cin>>percent;
+    
+    if(percent>=0 && percent<40){
+        cout<<"You are failed";
+    }
+    
+    else if(percent>=40 && percent<65){
+        cout<<"Fair";
+    }
+    
+    else if(percent>=65 && percent<75){
+        cout<<"Good";
+    }
+    
+    else if (percent>=75 && percent<=100){
+        cout<<"Excellent";
+    }
+    
+    else{
+        cout<<"invalid input";
+    }
+}
