04c44982ffc8c3a5ce7b1b15364c5c4ba8cf47e2
MaithilyKawale <maithilyworkspace@gmail.com>
2025-12-27 19:10:00 +0530

    code added

--- if else/cityandmonument.cpp
+++ if else/cityandmonument.cpp
@@ -2,3 +2,29 @@
 delhi red fort
 agra taj mahal
 jaipur jal mahal*/
+
+
+#include <iostream>
+using namespace std; 
+
+int main() {
+    string city;
+    cout<<"Enter name of city:";
+    cin>>city;
+    
+    if(city=="Delhi" || city=="delhi"){
+        cout<<"Red Fort";
+    }
+    
+    else if(city=="Agra" || city=="agra"){
+        cout<<"Taj Mahal";
+    }
+    
+    else if(city=="Jaipur" || city=="jaipur"){
+        cout<<"Jal Mahal";
+    }
+    
+    else{
+        cout<<"not known";
+    }
+}
