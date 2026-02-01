97ae14ed3bc90db8b4a0e229d2816a0ab9ab2061
MaithilyKawale <maithilyworkspace@gmail.com>
2026-02-01 15:55:58 +0530

    finding reverse range of number code added

---
+++ for loop/reverserange.java
@@ -0,0 +1,14 @@
+/*Display numbers from -10 to -1 using for loop*/
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        int i;
+        
+        for(i = -10; i <= -1; i++) {
+        System.out.println(i);
+    }
+    }
+}
+
