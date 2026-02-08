2d4e4a5b1e570c38978dc0045b588c6f33680a78
MaithilyKawale <maithilyworkspace@gmail.com>
2026-02-08 23:48:39 +0530

    reverse of numbers code added

---
+++ for loop/reverserange.c
@@ -0,0 +1,16 @@
+/*Display numbers from -10 to -1 using for loop*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    printf("Numbers from -10 to -1 are:\n");
+
+    for(i = -10; i <= -1; i++) {
+        printf("%d\n ", i);
+    }
+
+    return 0;
+}
+
