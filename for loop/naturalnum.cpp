d84f0616efd6063a109eb451a9887c92e0de5d56
MaithilyKawale <maithilyworkspace@gmail.com>
2026-02-15 10:31:45 +0530

    code added to find natural number

---
+++ for loop/naturalnum.cpp
@@ -0,0 +1,14 @@
+/* Print first 10 natural numbers using loop*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+
+    for(i = 1; i <= 10; i++) {
+        cout<<i<<endl;
+    }
+  
+    return 0;
+}
