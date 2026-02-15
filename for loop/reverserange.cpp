c20eb3eafdfbf914b2e58fd309951e2bf5e7d538
MaithilyKawale <maithilyworkspace@gmail.com>
2026-02-15 10:33:09 +0530

    code added to find reverse of number

---
+++ for loop/reverserange.cpp
@@ -0,0 +1,17 @@
+/*Display numbers from -10 to -1 using for loop*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+
+    cout<<"Numbers from -10 to -1 are:\n";
+
+    for(i = -10; i <= -1; i++) {
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
+
